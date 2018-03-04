cbd6833232d4bf97208601973bd5d35589ffc8d3
Michael Glauser <michael.glauser@ftr.com>
2018-03-03 18:24:23 -0800

    Will now import into DV868UVE v1.24 ContactList.

--- bin/app.js
+++ bin/app.js
@@ -12,11 +12,14 @@ request('http://www.dmr-marc.net/cgi-bin/trbo-database/datadump.cgi?table=users&
     var result = JSON.parse(body);
     if (result && result.users) {
       var wstream = fs.createWriteStream(outFileName);
+      // Write header:
+      wstream.write('"No.","Radio ID","Callsign","Name","City","State","County","Remarks","Call Type","Call Alert"\r\n');
       var index = 0;
       result.users.forEach(user => {
         index += 1;
         // {"country":"United States","callsign":"WD6AYE","name":"Michael J","radio_id":"3116173","surname":"Glauser","state":"Idaho","city":"Coeur D Alene","remarks":"DMR"}
-        var line = util.format('%d,%s,,%s %s,%s,%s,,"Private Call","None"\n', index, user.radio_id, user.callsign, user.name, user.city, user.state);
+        // "No.","Radio ID","Callsign","Name","City","State","County","Remarks","Call Type","Call Alert"
+        var line = util.format('"%d","%s","","%s %s","%s","%s","","","Private Call","None"\r\n', index, user.radio_id, user.callsign, user.name, user.city, user.state);
         wstream.write(line);
       });
       wstream.end();
